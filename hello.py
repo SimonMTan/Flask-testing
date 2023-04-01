65fbf3015443934315564b869f4a4d50150974a6
Simon <simon.tan09@gmail.com>
2023-03-31 18:48:01 -0700

    update CRUD

--- hello.py
+++ hello.py
@@ -1,7 +1,39 @@
-from flask import Flask
+from flask import Flask, request,jsonify
 
 app = Flask(__name__)
 
-@app.route("/")
+data = [{'name': 'alice',
+        'email': 'alice@outlook.com'}]
+
+@app.route('/')
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return jsonify(data)
+
+@app.route('/',methods=['PUT'])
+def create_world():
+    newworld = json.loads(request.data)
+    data.append(newworld)
+    return jsonify(data)
+
+@app.route('/',methods=['POST'])
+def update_page():
+    updateworld = json.loads(request.data)
+    for each in data:
+        if each['name'] === updateworld['name']
+            each['email'] = updateworld['email']
+        else:
+            continue
+    return jsonify(data)
+
+@app.route('/',methods=['Delete'])
+def delete_page():
+    deleteworld = json.loads(request.data)
+    newdata = []
+    for idx in range(len(data)):
+        each = data[idx]
+        if each['name'] != deleteworld['name']:
+            newdata.append(each)
+        else:
+            continue
+    data = newdata
+    return jsonify(data)
